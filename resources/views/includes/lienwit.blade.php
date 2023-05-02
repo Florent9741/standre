b360aa4da6b6d2af732e6693c10d35808478efa8
Florent9741 <florent974.lamoly@gmail.com>
2023-05-03 00:08:13 +0200

    mardi soir

--- resources/views/includes/lienwit.blade.php
+++ resources/views/includes/lienwit.blade.php
@@ -3,30 +3,249 @@
 
 <!-- component -->
 <!-- This is an example component -->
+<div
+    class="relative overflow-hidden bg-cover bg-green-400 bg-no-repeat "
+    style="
+      background-position: 50%;
+        
+      height: 115px;
+    ">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed "
+      style="background-color: rgba(0, 0, 0, 0.75)">
+      <div class="flex h-full items-center justify-center">
+        <div class="px-6 text-center text-white md:px-12">
+       
+          <h3 class="mb-8 text-4xl text-white py-10 ">Liens Wit</h3>
+          
+        </div>
+      </div>
+    </div>
+  </div>
 
-<div class='flex items-center justify-center min-h-screen'>  
+
+  <div
+    class="relative overflow-hidden bg-cover bg-blue-400 bg-no-repeat "
+    style="
+      background-position: 50%;
+        
+      height: 120px;
+    ">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed "
+      style="background-color: rgba(0, 0, 0, 0.75)">
+      <div class="flex h-full items-center justify-center">
+        <div class="px-6 text-center text-white md:px-12">
+      
+          <h3 class="mb-8 text-3xl text-white py-10 ">Actifs</h3>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class='flex items-center justify-center my-[120px] '>  
+   
+
+   <div class="mt-4 mb-6">
+     <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+     <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+     <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+   </div>
+
+  
+     
+</div>
+<div class='flex items-center justify-center my-[120px] '>  
    
 
     <div class="mt-4 mb-6">
-      <div class="mb-3 text-2xl font-bold">Mairie</div>
-      <div class="text-xl text-neutral-600">depuis la mairie : <a href="http://192.168.59.230" >http://192.168.59.230</a></div>
-      <div class="text-xl text-neutral-600 py-2">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" >https://webmail.ville-saint-andre.fr:8058</a></div>
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
     </div>
 
    
       
 </div>
-<div class='flex items-center justify-center min-h-screen '>  
+
+<div class='flex items-center justify-center my-[120px] '>  
    
 
     <div class="mt-4 mb-6">
-      <div class="mb-3 text-2xl font-bold">Mairie</div>
-      <div class="text-xl text-neutral-600">depuis la mairie : <a href="http://192.168.59.230" >http://192.168.59.230</a></div>
-      <div class="text-xl text-neutral-600 py-2">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" >https://webmail.ville-saint-andre.fr:8058</a></div>
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
     </div>
 
    
       
 </div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+    <div class="mt-4 mb-6">
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+    </div>
+
+   
+      
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+    <div class="mt-4 mb-6">
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+    </div>
+
+   
+      
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+    <div class="mt-4 mb-6">
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+    </div>
+
+   
+      
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+    <div class="mt-4 mb-6">
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+    </div>
+
+   
+      
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+    <div class="mt-4 mb-6">
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+    </div>
+
+   
+      
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+    <div class="mt-4 mb-6">
+      <div class="mb-3 text-2xl text-blue-800 italic text-center ">Mairie</div>
+      <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="http://192.168.59.230" class="text-xl underline" >http://192.168.59.230</a></div>
+      <div class="text-base text-neutral-600 py-2 text-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8058" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8058</a></div>
+    </div>
+
+   
+      
+</div>
+<div
+    class="relative overflow-hidden bg-cover bg-blue-400 bg-no-repeat "
+    style="
+      background-position: 50%;
+        
+      height: 120px;
+    ">
+    <div
+      class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed "
+      style="background-color: rgba(0, 0, 0, 0.75)">
+      <div class="flex h-full items-center justify-center">
+        <div class="px-6 text-center text-white md:px-12">
+      
+          <h3 class="mb-8 text-3xl text-white py-10 ">A venir...</h3>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='flex items-center justify-center my-[120px] '>  
+   
+
+   <div class="mt-4 mb-6">
+     <div class="mb-3 text-2xl text-blue-800 italic text-center ">Environnement </div>
+     <p class="text-center text-blue-900 text-xs">
+     (à rattacher à une des prises disponibles sur le switch du bureau de Rachid, 1er étage)
+     </p>
+     <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="https://192.168.61.193" class="text-xl underline" >https://192.168.61.193</a></div>
+     <div class="text-base text-neutral-600 py-2 text-center flex nowrap items-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8054" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8054  </a> <p class="px-3 text-center text-xs text-red-500">(à configurer)</p></div>
+   </div>
+
+  
+     
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+   <div class="mt-4 mb-6">
+     <div class="mb-3 text-2xl text-blue-800 italic text-center ">Environnement </div>
+     <p class="text-center text-blue-900 text-xs">
+     (à rattacher à une des prises disponibles sur le switch du bureau de Rachid, 1er étage)
+     </p>
+     <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="https://192.168.61.193" class="text-xl underline" >https://192.168.61.193</a></div>
+     <div class="text-base text-neutral-600 py-2 text-center flex nowrap items-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8054" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8054  </a> <p class="px-3 text-center text-xs text-red-500">(à configurer)</p></div>
+   </div>
+
+  
+     
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+   <div class="mt-4 mb-6">
+     <div class="mb-3 text-2xl text-blue-800 italic text-center ">Environnement </div>
+     <p class="text-center text-blue-900 text-xs">
+     (à rattacher à une des prises disponibles sur le switch du bureau de Rachid, 1er étage)
+     </p>
+     <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="https://192.168.61.193" class="text-xl underline" >https://192.168.61.193</a></div>
+     <div class="text-base text-neutral-600 py-2 text-center flex nowrap items-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8054" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8054  </a> <p class="px-3 text-center text-xs text-red-500">(à configurer)</p></div>
+   </div>
+
+  
+     
+</div>
+
+<div class='flex items-center justify-center my-[120px] '>  
+   
+
+   <div class="mt-4 mb-6">
+     <div class="mb-3 text-2xl text-blue-800 italic text-center ">Environnement </div>
+     <p class="text-center text-blue-900 text-xs">
+     (à rattacher à une des prises disponibles sur le switch du bureau de Rachid, 1er étage)
+     </p>
+     <div class="text-base text-neutral-600 text-center">Depuis la mairie : <a href="https://192.168.61.193" class="text-xl underline" >https://192.168.61.193</a></div>
+     <div class="text-base text-neutral-600 py-2 text-center flex nowrap items-center">Depuis l'extérieur : <a href="https://webmail.ville-saint-andre.fr:8054" class="text-xl underline" >https://webmail.ville-saint-andre.fr:8054  </a> <p class="px-3 text-center text-xs text-red-500">(à configurer)</p></div>
+   </div>
+
+  
+     
+</div>
+
+
+
+
 
 @endsection
