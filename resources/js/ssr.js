648aa3f86f37c32d4fc17d50db50f38568a433b6
Florent9741 <florent974;lamoly@gmail.com>
2023-05-04 14:55:59 +0200

    deploiement local

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-server: {
-    host: true
-}
-
+ 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: 'resources/js/app.js',
+            ssr: 'resources/js/ssr.js',
         }),
     ],
-    
 });
